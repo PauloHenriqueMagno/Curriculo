0119d94b54cba8a4d7b3510a1c25d8e30fa06832
PauloHenriqueMagno <paulohm2309@gmail.com>
2022-03-27 15:45:33 -0300

    feat: add circles component

---
+++ src/components/Circles/index.jsx
@@ -0,0 +1,15 @@
+import { DivCirclesStyled } from "./styled";
+
+const CirclesSkillsSectionComponent = () => {
+  return (
+    <DivCirclesStyled>
+      <svg width="100%" height="100%" viewBox="0 0 665 118" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M568.589 47.62C568.589 21.3202 590.2 0 616.5 0V0C642.799 0 664.41 21.3202 664.41 47.62V47.62C664.41 73.9198 642.799 95.24 616.5 95.24V95.24C590.2 95.24 568.589 73.9198 568.589 47.62V47.62Z" fill="#EDDBFF"/>
+        <path d="M0 70.5C0 44.2665 21.5542 23 47.7878 23V23C74.0213 23 95.5755 44.2665 95.5755 70.5V70.5C95.5755 96.7335 74.0213 118 47.7878 118V118C21.5542 118 0 96.7335 0 70.5V70.5Z" fill="#FFC6C6"/>
+        <path d="M224.354 54.55C224.354 37.6943 238.204 24.03 255.06 24.03V24.03C271.916 24.03 285.767 37.6943 285.767 54.55V54.55C285.767 71.4058 271.916 85.07 255.06 85.07V85.07C238.204 85.07 224.354 71.4058 224.354 54.55V54.55Z" fill="#6970B4"/>
+      </svg>
+    </DivCirclesStyled>
+  );
+};
+
+export default CirclesSkillsSectionComponent;
---
+++ src/components/Circles/styled.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const DivCirclesStyled = styled.div`
+  position: absolute;
+  width: 85%;
+  max-width: 1100px;
+  left: 50%;
+  transform: translate(-50%, -36%);
+  z-index: -1;
+`;
