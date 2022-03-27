2dfbfc1bc8ac3458b86fbaade18fa63bbaafdae3
PauloHenriqueMagno <paulohm2309@gmail.com>
2022-03-27 15:48:16 -0300

    add components to app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import HeaderComponent from './components/Header';
-import ProfileSection from './components/ProfileSection';
+import ProfileSectionComponent from './components/ProfileSection';
+import SkillsSectionComponent from './components/SkillsSection';
+import ProjectsSectionComponent from './components/ProjectsSection'
 
 function App() {
   return (
     <>
       <HeaderComponent />
-      <ProfileSection />
+      <ProfileSectionComponent />
+      <SkillsSectionComponent />
+      <ProjectsSectionComponent />
     </>
   );
 }
