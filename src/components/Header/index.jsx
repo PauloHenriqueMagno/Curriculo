5cad42f2a629d8a4e10b243e40ecf6695d62603d
PauloHenriqueMagno <paulohm2309@gmail.com>
2022-03-27 15:43:42 -0300

    feat: improve header e fix layout

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,29 +1,56 @@
+import ContainerComponent from "../Container"
 import {
+  HeaderStyled,
+  HeaderDivStyled,
   HeaderNavStyled,
-  HeaderNavButtonStyled
+  HeaderNavLinkStyled
 } from './styled';
-import { useThemeContext } from '../../provider/theme';
+
+import { useDispatch, useSelector } from "react-redux";
+import { setLanguageThunk } from "../../store/modules/language/thunks";
 
 const HeaderComponent = () => {
-  const { theme } = useThemeContext()
+  const { content, theme } = useSelector(state => state);
+  const { data, language } = content;
+  const dispatch = useDispatch();
+
   return (
-    <HeaderNavStyled>
-      <HeaderNavButtonStyled color={theme.purple}>
-        Perfil
-      </HeaderNavButtonStyled>
-      <HeaderNavButtonStyled color={theme.blue}>
-        Habilidades
-      </HeaderNavButtonStyled>
-      <HeaderNavButtonStyled color={theme.green}>
-        Projetos
-      </HeaderNavButtonStyled>
-      <HeaderNavButtonStyled color={theme.purpleDark}>
-        Certificados
-      </HeaderNavButtonStyled>
-      <HeaderNavButtonStyled color={theme.yellow}>
-        Contato
-      </HeaderNavButtonStyled>
-    </HeaderNavStyled>
+    <HeaderStyled>
+      <ContainerComponent>
+        <HeaderNavStyled>
+          <HeaderDivStyled>
+            <select
+              value = {language}
+              onChange = {(e) => dispatch(setLanguageThunk(e.currentTarget.value))}
+            >
+              <option value={"EN"}>English</option>
+              <option value={"PT_BR"} defaultChecked>Português</option>
+            </select>
+
+            <HeaderNavLinkStyled color={theme.purple} href="#about">
+              {data.titles.about}
+            </HeaderNavLinkStyled>
+
+            <HeaderNavLinkStyled color={theme.blue} href="#skills">
+              {data.titles.skills}
+            </HeaderNavLinkStyled>
+
+            <HeaderNavLinkStyled color={theme.green} href="#projects">
+              {data.titles.projects}
+            </HeaderNavLinkStyled>
+
+            <HeaderNavLinkStyled color={theme.wine} href="#certifications">
+              {data.titles.certifications}
+            </HeaderNavLinkStyled>
+
+            <HeaderNavLinkStyled color={theme.yellow} href="#contact">
+              {data.titles.contact}
+            </HeaderNavLinkStyled>
+
+          </HeaderDivStyled>
+        </HeaderNavStyled>
+      </ContainerComponent>
+    </HeaderStyled>
   );
 };
 
--- src/components/Header/styled.js
+++ src/components/Header/styled.js
@@ -10,30 +10,70 @@ const buttonAnimation = (color) => keyframes`
   }
 `;
 
-export const HeaderNavStyled = styled.nav`
+export const HeaderStyled = styled.header`
   display: flex;
   position: sticky;
   top: 0;
   background-color: rgb(255,255,255);
-  width: 100vw;
-  height: 70px;
-  align-items: center;
-  justify-content: flex-end;
-  border: 1px solid black;
-  gap: 25px;
-  padding: 10px 55px;
+  width: 100%;
+  max-width: 100vw;
+  z-index: 100;
 `;
 
-export const HeaderNavButtonStyled = styled.button`
-  background: none;
-  padding: 20px;
+export const HeaderNavStyled = styled.nav`
+  overflow-x: auto;
+  overflow-y: hidden;
+  width: 100%;
+`;
+
+export const HeaderDivStyled = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  width: max-content;
+  min-width: 100%;
+  padding: 0 5px;
+  height: 60px;
+  gap: 15px;
+  
+  @media (min-width: 600px){
+    justify-content: flex-end;
+  };
+`;
+
+export const HeaderNavLinkStyled = styled.a`
+  background-color: ${props => props.color};
+  padding: 10px 10px;
   border-radius: 100%;
-  transition-duration: 700ms;
+  font-size: 0.8rem;
   font-weight: 600;
   cursor: pointer;
   
-  &:hover {
-    animation: ${props => buttonAnimation(props.color)} 700ms;
-    background-color: ${props => props.color};
-  }
+  &, &:active, &:visited {
+    outline: none;
+    outline-color: none;
+    text-decoration: none;
+    color: #000000;
+  };
+
+  &:active {
+    filter: brightness(70%)
+  };
+  
+  @media (min-width: 600px){
+    background: none;
+
+    &:hover {
+      animation: ${props => buttonAnimation(props.color)} 700ms;
+      background-color: ${props => props.color};
+    }
+  };
+`;
+
+export const HeaderLanguageSelectStyled = styled.select`
+  &:hover, &:focus, &:active {
+    outline: none;
+    outline-color: none;
+    -webkit-tap-highlight-color: rgba(0,0,0,0);
+  };
 `;
